7ed6bb45c158bdb4509f10ecddc48d030b283703
Gregory Subero <suberog010@gmail.com>
2026-01-04 22:07:16 -0400

    cambios strapi lib

--- frontend/src/lib/strapi.js
+++ frontend/src/lib/strapi.js
@@ -1,7 +1,7 @@
 import qs from 'qs'; 
 import { cacheLife } from 'next/cache';
 
-export const STRAPI_BASE_URL = "http://localhost:1337";
+export const STRAPI_BASE_URL = process.env.STRAPI_BASE_URL || "http://localhost:1337";
 
 const QUERY_HOME_PAGE = {
   populate: {
